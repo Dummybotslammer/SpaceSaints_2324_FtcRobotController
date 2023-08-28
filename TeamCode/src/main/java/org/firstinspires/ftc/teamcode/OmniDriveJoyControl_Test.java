52a5f7dd55db93afdc4799d5d867c2d733210393
DummDank <gunzhongwen@gmail.com>
2023-08-29 00:38:37 +0800

    Created basis for the project. Will refactor soon. Maybe.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDriveJoyControl_Test.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.omnidrive.OmniDriveController;
+
+import javax.vecmath.Vector2d;
+
+@TeleOp(name="OmniBot Drivebase Joystick Control Test")
+public class OmniDriveJoyControl_Test extends LinearOpMode {
+    //All physical measurements are in SI units, unless stated otherwise, or being converted to motor/encoder ticks.
+    //Angles are all in radians unless specified otherwise as well.
+    private final int TICKS_PER_HDHEXMOTOR_REV = 560;
+    private final int TICKS_PER_COREHEXMOTOR_REV = 288;
+    private final double wheelDiameter = 0.09;
+    private final double stickMovementVectorScaleFactor = 0.2; //For: heading_velocity = scale * velocity (ms^-1)
+    private final double maxAngularVelocity = Math.PI/3;
+
+    private Blinker control_Hub;
+    private Gyroscope imu;
+    private DcMotorEx drive1;
+    private DcMotorEx drive2;
+    private DcMotorEx drive3;
+    private OmniDriveController omnidrive;
+
+    @Override
+    public void runOpMode() {
+        drive1 = hardwareMap.get(DcMotorEx.class, "drive1");
+        drive2 = hardwareMap.get(DcMotorEx.class, "drive2");
+        drive3 = hardwareMap.get(DcMotorEx.class, "drive3");
+
+        telemetry.addData("Status", "Running");
+
+        //RUNTIME VARIABLES
+        double leftStickX;
+        double leftStickY;
+        double rightStickX;
+        double angularVelocity;
+        Vector2d stickVector = new Vector2d(0, 0);
+        Vector2d heading_velocity;
+
+        //MOTOR & ENCODER RESETS AND CONFIGS
+        drive1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drive2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drive3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        drive1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        drive1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drive2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drive3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        omnidrive = new OmniDriveController(drive1, drive2, drive3, TICKS_PER_HDHEXMOTOR_REV, TICKS_PER_HDHEXMOTOR_REV, TICKS_PER_COREHEXMOTOR_REV, wheelDiameter);
+        waitForStart();
+
+        while(opModeIsActive()) {
+            leftStickX = gamepad1.left_stick_x;
+            leftStickY = -1*gamepad1.left_stick_y;
+            rightStickX = gamepad1.right_stick_x;
+            angularVelocity = maxAngularVelocity*rightStickX;
+
+            stickVector.set(new double[]{leftStickX, leftStickY});
+            stickVector.scale(stickMovementVectorScaleFactor);
+            omnidrive.setAngularVelocity(angularVelocity);
+            omnidrive.setHeading_velocity(stickVector);
+
+            omnidrive.computeVelocitiesThenDrive();
+
+            double[] t1 = {omnidrive.getDriveVelocities()[0].x,omnidrive.getDriveVelocities()[0].y};
+            double[] t2 = {omnidrive.getDriveVelocities()[1].x,omnidrive.getDriveVelocities()[1].y};
+            double[] t3 = {omnidrive.getDriveVelocities()[2].x,omnidrive.getDriveVelocities()[2].y};
+            telemetry.addData("Left Stick X", leftStickX);
+            telemetry.addData("Left Stick Y", leftStickY);
+            telemetry.addData("Right Stick X", rightStickX);
+            telemetry.addData("Drive1 Velocity", String.format("%f, %f", t1[0], t1[1]));
+            telemetry.addData("Drive2 Velocity", String.format("%f, %f", t2[0], t2[1]));
+            telemetry.addData("Drive3 Velocity", String.format("%f, %f", t3[0], t3[1]));
+            telemetry.addData("Drive1 Speed", omnidrive.getDriveSpeeds()[0]);
+            telemetry.addData("Drive2 Speed", omnidrive.getDriveSpeeds()[1]);
+            telemetry.addData("Drive3 Speed", omnidrive.getDriveSpeeds()[2]);
+            telemetry.addData("Drive1 Vector Angle", Math.toDegrees(omnidrive.getDriveVelocities()[0].angle(omnidrive.getUnitVectors()[0])));
+            telemetry.addData("Drive2 Vector Angle", Math.toDegrees(omnidrive.getDriveVelocities()[0].angle(omnidrive.getUnitVectors()[0])));
+            telemetry.addData("Drive3 Vector Angle", Math.toDegrees(omnidrive.getDriveVelocities()[0].angle(omnidrive.getUnitVectors()[0])));
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omnidrive/OmniDriveController.java
@@ -0,0 +1,136 @@
+package org.firstinspires.ftc.teamcode.omnidrive;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.omnidrive.OmniDriveController;
+
+import java.lang.Math;
+import javax.vecmath.Vector2d;
+
+public class OmniDriveController {
+    //The OmniDrive runs on 3 different motors.
+    //The motors need to be set to DcMotor.RunMode.RUN_USING_ENCODER mode.
+    //All physical quantities are in SI units, which are then converted to the encoder counts for the purpose of the motors.
+    private DcMotorEx drive1;
+    private DcMotorEx drive2;
+    private DcMotorEx drive3;
+
+    private Vector2d heading_velocity = new Vector2d(0,0);
+    private Vector2d drive1_velocity = new Vector2d(0,0);
+    private Vector2d drive2_velocity = new Vector2d(0,0);
+    private Vector2d drive3_velocity = new Vector2d(0,0);
+    private double drive1_speed = 0.0;
+    private double drive2_speed = 0.0;
+    private double drive3_speed = 0.0;
+    private double angularVelocity = 0.0;
+
+    //Unit Vectors with length 1.
+    private Vector2d drive1_unitVector = new Vector2d(0.5, Math.sqrt(0.75));
+    private Vector2d drive2_unitVector = new Vector2d(0.5, -Math.sqrt(0.75));
+    private Vector2d drive3_unitVector = new Vector2d(-1.0, 0.0);
+
+    private double wheelDiameter = 0.09;
+    private double inscribedBasedRadius = Math.sqrt(14699.905641)/10/100; //mm -> cm -> m
+    private int TICKS_PER_DRIVE1_REV = 28;
+    private int TICKS_PER_DRIVE2_REV = 28;
+    private int TICKS_PER_DRIVE3_REV = 28;
+
+    //Constructor
+    public OmniDriveController(DcMotorEx motor1, DcMotorEx motor2, DcMotorEx motor3) {
+        drive1 = motor1;
+        drive2 = motor2;
+        drive3 = motor3;
+    }
+
+    public OmniDriveController(DcMotorEx motor1, DcMotorEx motor2, DcMotorEx motor3, int TICKS_PER_MOTOR1_REV, int TICKS_PER_MOTOR2_REV, int TICKS_PER_MOTOR3_REV, double wheel_diameter) {
+        drive1 = motor1;
+        drive2 = motor2;
+        drive3 = motor3;
+
+        TICKS_PER_DRIVE1_REV = TICKS_PER_MOTOR1_REV;
+        TICKS_PER_DRIVE2_REV = TICKS_PER_MOTOR2_REV;
+        TICKS_PER_DRIVE3_REV = TICKS_PER_MOTOR3_REV;
+
+        wheelDiameter = wheel_diameter;
+    }
+
+    //Setter/Getter Methods
+    public void setHeading_velocity(Vector2d vector) {
+        heading_velocity = vector;
+    }
+
+    public void setAngularVelocity(double w) {
+        angularVelocity = w;
+    }
+
+    public void setDriveMotors(DcMotorEx motor1, DcMotorEx motor2, DcMotorEx motor3) {
+        drive1 = motor1;
+        drive2 = motor2;
+        drive3 = motor3;
+    }
+
+    public Vector2d[] getDriveVelocities() {
+        return new Vector2d[]{drive1_velocity, drive2_velocity, drive3_velocity};
+    }
+
+    public Vector2d[] getUnitVectors() {
+        return new Vector2d[]{drive1_unitVector, drive2_unitVector, drive3_unitVector};
+    }
+
+    public double[] getDriveSpeeds() {
+        return new double[]{drive1_speed, drive2_speed, drive3_speed};
+    }
+
+    public Vector2d getHeading_velocity() {
+        return heading_velocity;
+    }
+
+    //Computation Methods
+    public void computeDriveVelFromHeadingAndAngularVel() {
+        Vector2d drive1_turnVel = new Vector2d(drive1_unitVector);
+        Vector2d drive2_turnVel = new Vector2d(drive2_unitVector);
+        Vector2d drive3_turnVel = new Vector2d(drive3_unitVector);
+        drive1_velocity = new Vector2d(drive1_unitVector);
+        drive2_velocity = new Vector2d(drive2_unitVector);
+        drive3_velocity = new Vector2d(drive3_unitVector);
+        double turnSpeed_per_motor = angularVelocity*inscribedBasedRadius;
+
+        drive1_turnVel.scale(turnSpeed_per_motor);
+        drive2_turnVel.scale(turnSpeed_per_motor);
+        drive3_turnVel.scale(turnSpeed_per_motor);
+
+        drive1_velocity.scale(drive1_unitVector.dot(heading_velocity));
+        drive2_velocity.scale(drive2_unitVector.dot(heading_velocity));
+        drive3_velocity.scale(drive3_unitVector.dot(heading_velocity));
+
+        drive1_velocity.add(drive1_turnVel);
+        drive2_velocity.add(drive2_turnVel);
+        drive3_velocity.add(drive3_turnVel);
+
+        //Boolean Logic: (angle > 0)? TRUE=-1:FALSE=1
+        //Returns -1 if the angle is positive, and 1 if the angle is 0 or less.
+        drive1_speed = drive1_velocity.length() * ((drive1_velocity.angle(drive1_unitVector) > 0)? -1:1);
+        drive2_speed = drive2_velocity.length() * ((drive2_velocity.angle(drive2_unitVector) > 0)? -1:1);
+        drive3_speed = drive3_velocity.length() * ((drive3_velocity.angle(drive3_unitVector) > 0)? -1:1);
+    }
+
+    //Movement Methods
+    public void drive() {
+        //Sets the robot's driving motor's velocities to the ALREADY computed values
+        //Resets the computed drive1_velocity, drive2_velocity, drive3_velocity values
+        int drive1_tickspeed = (int)(drive1_speed/wheelDiameter*TICKS_PER_DRIVE1_REV);
+        int drive2_tickspeed = (int)(drive2_speed/wheelDiameter*TICKS_PER_DRIVE2_REV);
+        int drive3_tickspeed = (int)(drive3_speed/wheelDiameter*TICKS_PER_DRIVE3_REV);
+
+        drive1.setVelocity(drive1_tickspeed);
+        drive2.setVelocity(drive2_tickspeed);
+        drive3.setVelocity(drive3_tickspeed);
+    }
+
+    public void computeVelocitiesThenDrive() {
+        //Recomputes the vectors, and then drives the motors.
+        computeDriveVelFromHeadingAndAngularVel();
+        drive();
+    }
+}
